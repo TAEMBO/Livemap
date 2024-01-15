051eeb84d016298b110edbe9ef83e0399f8806d2
TÆMBØ <kuneshr123@gmail.com>
2024-01-14 23:18:30 -0800

    Remove unnecessary endpoint

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -5,23 +5,21 @@ export default class APIRouter {
     public readonly router = express.Router();
 
     constructor(private readonly _app: App) {
-        this.router
-            .get('/geo.json', async (req, res) => {
-                const geos = [];
-                    
-                for (const obj of this._app.cachedEntities.vehicles) {
-                    if (obj.posx !== null) geos.push({
-                        type: 'Feature',
-                        geometry: {
-                            type: 'Point',
-                            coordinates: [obj.posx, obj.posy]
-                        },
-                        properties: obj
-                    });
-                }
-            
-                res.json({ type: "FeatureCollection", features: geos });
-            })
-            .get('/serverstats.json', async (req, res) => res.json({ 'careerSavegame': await this._app.fetchCSG() }));
+        this.router.get('/geo.json', async (req, res) => {
+            const geos = [];
+                
+            for (const obj of this._app.cachedEntities.vehicles) {
+                if (obj.posx !== null) geos.push({
+                    type: 'Feature',
+                    geometry: {
+                        type: 'Point',
+                        coordinates: [obj.posx, obj.posy]
+                    },
+                    properties: obj
+                });
+            }
+        
+            res.json({ type: "FeatureCollection", features: geos });
+        });
     }
 }
