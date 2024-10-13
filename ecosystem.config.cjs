96fa49991c189d36f533c79b9d41488bef4da03b
TAEMBO <kuneshr123@gmail.com>
2024-10-12 21:14:34 -0700

    chore: Prefer Node.js `env-file` flag over dotenv

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,15 +1,9 @@
-require("dotenv/config");
-
-// We use this file when working with PM2 for compatibility when loading ENV variable(s)
-// as a replacement for `npm run start`
 module.exports = {
 	apps: [
 		{
 			name: "Livemap",
 			script: "build/index.js",
-			env: {
-				PORT: process.env.PORT
-			}
+			interpreter_args: "--env-file=.env"
 		}
 	]
 }
