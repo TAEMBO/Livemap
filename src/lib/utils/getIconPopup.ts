c065d5d41a99e8cfd763fd12ed467fd2aec6a4dc
TAEMBO <kuneshr123@gmail.com>
2025-02-24 13:20:12 -0800

    fix: Missing fills info

--- src/lib/utils/getIconPopup.ts
+++ src/lib/utils/getIconPopup.ts
@@ -9,16 +9,16 @@ export function getIconPopup(vehicle: Vehicle) {
         : vehicle.fills.filter(fill => !["DIESEL", "DEF", "AIR"].includes(fill.type));
 
     if (fillData && fillData.length) { // Equipment has relevant fills
-        if (fillData.some(fill => fill.type === "UNKNOWN")) { // Empty fill
+        if (fillData.some(fill => fill.type === "UNKNOWN") && fillData.length === 1) { // Empty fill
             popupHTML += "<br>Empty";
         } else { // Valid & relevant fill data
-            popupHTML += fillData.map(fill => {
+            popupHTML += fillData.filter(fill => fill.type !== "UNKNOWN").map(fill => {
                 return `<br><span style="text-transform: capitalize;">${fill.type.toLowerCase()}</span> (${fill.level.toLocaleString("en-US")})`;
             }).join("");
         }
     }
 
-    if (vehicle.controller) popupHTML += `<br>Player: ${vehicle.controller}`;
+    if ("controller" in vehicle) popupHTML += `<br>Player: ${vehicle.controller}`;
 
     popupHTML += "</small>";
 
