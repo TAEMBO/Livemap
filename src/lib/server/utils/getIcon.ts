18560b571f56f6b9cf853f4fa1fa46930b9ce9d4
TAEMBO <kuneshr123@gmail.com>
2025-08-15 00:48:00 -0700

    refactor: Icon assignment

--- src/lib/server/utils/getIcon.ts
+++ src/lib/server/utils/getIcon.ts
@@ -2,9 +2,7 @@ import type { Vehicle } from "farming-simulator-types/2025";
 import { icons } from "./icons";
 
 export function getIcon(object: Vehicle) {
-    let icon = icons[object.category.toLowerCase()];
-
-    icon ??= icons[object.type.toLowerCase()];
+    let icon = icons[object.type];
 
     if ("controller" in object) icon = object.controller?.length ? icons.controller : icons.controllerghost;
 
